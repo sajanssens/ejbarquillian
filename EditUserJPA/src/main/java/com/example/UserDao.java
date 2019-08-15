12cd213cb1b9b7f8fee1f35315ebbec9701347d5
bramjanssens <bram.janssens@infosupport.com>
2019-08-15 16:39:03 +0200

    Some attempts to make ik work...

---
+++ EditUserJPA/src/main/java/com/example/UserDao.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+
+@Stateful
+public class UserDao implements Serializable {
+
+    @PersistenceContext(name = "MyPersistenceUnit")
+    private EntityManager entityManager;
+
+    public void save(User user) {
+        System.out.println("UserDao.save");
+        // entityManager.persist(user);
+    }
+
+}
---
+++ EditUserJPA/src/main/java/com/example/UserEdit.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@ViewScoped
+@Named // now it can be used in a JSF with EL
+public class UserEdit implements Serializable {
+
+    private User user = new User();
+
+    @Inject
+    private UserDao userDAO;
+
+    public String save() {
+        userDAO.save(this.user);
+        System.out.println("UserEdit.save");
+        return "index.jsp";
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
