d4e62851c82f69813526666c83513dcb4f56c4be
bramjanssens <bram.janssens@infosupport.com>
2019-08-21 13:57:51 +0200

    Cleaned.

--- EditUser/src/main/java/com/example/User.java
+++ EditUser/src/main/java/com/example/User.java
@@ -1,16 +1,9 @@
 package com.example;
 
-import javax.persistence.*;
 import javax.validation.constraints.Size;
 
-import static javax.persistence.GenerationType.IDENTITY;
-
-@Entity
-@NamedQueries({@NamedQuery(name = "com.example.User.getAll", query = "SELECT u FROM User u")})
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
     private Integer id;
 
     @Size(min = 2, message = "First name too short")
@@ -18,14 +11,6 @@ public class User {
 
     @Size(min = 2, message = "Last name too short")
     private String lastName;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
