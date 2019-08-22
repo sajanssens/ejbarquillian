7acc1d60b4ecdcf50303d69129344b4d4057a228
bramjanssens <bram.janssens@infosupport.com>
2019-08-22 15:05:19 +0200

    First commit.

---
+++ HelloArquillian/test/GreeterTest.java
@@ -0,0 +1,33 @@
+import com.example.hello.Greeter;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+@RunWith(Arquillian.class)
+public class GreeterTest {
+
+    // See https://www.jetbrains.com/help/idea/arquillian.html#a6afa129 for configuring IDEA
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(Greeter.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject Greeter greeter;
+
+    @Test
+    public void greet() throws Exception {
+        String name = "Arquillian";
+        Assert.assertEquals("Hello, Arquillian!", greeter.createGreeting(name));
+        greeter.greet(System.out, name);
+    }
+}
