fc59486bc6e09197a3ae6898e5a72f67be8172cb
bramjanssens <bram.janssens@infosupport.com>
2019-08-26 14:18:01 +0200

    Some refactoring.

--- helloarquillianmaven/src/main/java/com/example/hello/Greeter.java
+++ helloarquillianmaven/src/main/java/com/example/hello/Greeter.java
@@ -1,7 +1,9 @@
 package com.example.hello;
 
+import javax.ejb.Stateless;
 import java.io.PrintStream;
 
+@Stateless
 public class Greeter {
     public void greet(PrintStream to, String name) {
         to.println(createGreeting(name));
--- helloarquillianmaven/src/test/java/com/example/hello/Test.java
+++
@@ -1,9 +0,0 @@
-package com.example.hello;
-
-public class Test {
-
-    @org.junit.Test
-    public void name() {
-        System.out.println("boe");
-    }
-}
